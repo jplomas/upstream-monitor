92b9541d822bdb06e42161bbc9d2d9b56a975efd
JP Lomas <jp@theqrl.org>
2024-09-25 18:16:08 +0100

    commit grabbing & todos
    Signed-off-by: JP Lomas <jp@theqrl.org>

--- start.js
+++ start.js
@@ -6,13 +6,27 @@ async function start() {
   const file = fs.readFileSync('./config.yml', 'utf8');
   const config = YAML.parse(file);
   for (const upstream of config.upstreams) {
+    // TODO: auth to GitHub API
+    // TODO: use `since` to get commits since last successful check
     console.log(`getting issues from GitHub for: ${upstream.org}/${upstream.repo} on branch ${upstream.branch}`);
     const octokit = new Octokit({});
     const issues = await octokit.paginate('GET /repos/{owner}/{repo}/issues', {
       owner: upstream.org,
       repo: upstream.repo,
     });
+    console.log('==== ISSUES ====');
     console.log(issues);
+    //
+    console.log(`getting commits from GitHub for: ${upstream.org}/${upstream.repo} on branch ${upstream.branch}`);
+    const commits = await octokit.paginate('GET /repos/{owner}/{repo}/commits', {
+      owner: upstream.org,
+      repo: upstream.repo,
+      sha: upstream.branch,
+    });
+    console.log('==== COMMITS ====');
+    console.log(commits);
+    // TODO: compare commits and issues to last
+    // TODO: write last commits and issues to markdown files
   }
 }
 
