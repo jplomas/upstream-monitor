2c01dde142a2c0776b205c1676df3b05162cb74e
JP Lomas <jp@theqrl.org>
2024-10-04 10:53:03 +0100

    Use .env and github token
    Signed-off-by: JP Lomas <jp@theqrl.org>

--- start.js
+++ start.js
@@ -1,21 +1,28 @@
 import YAML from 'yaml';
 import fs from 'fs';
 import { Octokit } from '@octokit/rest';
+import dotenv from 'dotenv';
+dotenv.config();
 
 async function start() {
   const file = fs.readFileSync('./config.yml', 'utf8');
   const config = YAML.parse(file);
   for (const upstream of config.upstreams) {
     // TODO: auth to GitHub API
+
     // TODO: use `since` to get commits since last successful check
     console.log(`getting issues from GitHub for: ${upstream.org}/${upstream.repo} on branch ${upstream.branch}`);
-    const octokit = new Octokit({});
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN,
+    });
     const issues = await octokit.paginate('GET /repos/{owner}/{repo}/issues', {
       owner: upstream.org,
       repo: upstream.repo,
     });
     console.log('==== ISSUES ====');
-    console.log(issues);
+    issues.forEach((issue) => {
+      console.log(issue.title);
+    });
     //
     console.log(`getting commits from GitHub for: ${upstream.org}/${upstream.repo} on branch ${upstream.branch}`);
     const commits = await octokit.paginate('GET /repos/{owner}/{repo}/commits', {
@@ -24,7 +31,10 @@ async function start() {
       sha: upstream.branch,
     });
     console.log('==== COMMITS ====');
-    console.log(commits);
+    commits.forEach((commit) => {
+      // console.log(commit);
+      console.log(commit.commit.message);
+    });
     // TODO: compare commits and issues to last
     // TODO: write last commits and issues to markdown files
   }
